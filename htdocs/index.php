fb5c1b47d6718b01621c52f7acb568ca33790679
Bart Moorman <github@id10t.us>
2020-04-09 00:31:32 -0600

    Add favicon

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,6 +11,9 @@ $sunset = date('h:i A', date_sunset(time(), SUNFUNCS_RET_TIMESTAMP, $garage->ast
     <title>Garage - Index</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no'>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <?php require_once('include.css'); ?>
     <style>
       nav.navbar {
